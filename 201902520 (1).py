a1b6505a7591ae974440f6f156f1628f7325e299
LeeSY99 <101381138+LeeSY99@users.noreply.github.com>
2022-03-18 15:24:27 +0900

    Update 201902520 (1).py

--- 201902520 (1).py
+++ 201902520 (1).py
@@ -1,32 +1,44 @@
 
 #컴퓨터전자시스템공학부 201902520 이상윤
+#최대공약수와 최소공배수를 구하는 문제
 a,b,c = input().split()
 a,b,c = int(a), int(b), int(c)
-mul1=a*b   #a와b의 값이 변하기 전에 곱한값을 구해놓음
-a1=a			 #아래에서 값이 변하므로 미리 지정
-b1=b
-c2=c
+def gcd(x,y,z):         #최대공약수 구하는 함수
+    while x%y !=0:      #x,y의 최대공약수
+        temp= x%y
+        x = y
+        y = temp
+    xygys = y           
 
-while a1%b1 != 0:
-	temp = a1%b1
-	a1 = b1
-	b1 = temp
-midgys1=b1                    #유클리드 알고리즘을 이용하여 a와b의 최대공약수를 구함 midbys1로 저장.
+    while xygys % z !=0:     #x,y의 최대공약수와 z의 최대공약수
 
-midgbs1=mul1/midgys1          #유클리드 알고리즘으로 a와b의 최소공배수는 a*b의값에 최대공약수를 나눠주면 됨
+        temp= xygys % z
+        xygys = z
+        z= temp
+    xyzgcd=z
+    return xyzgcd      
+def lcm(x2,y2,z2):      #최소공배수 구하는 함수
+    x1=x2
+    y1=y2
+    z1=z2
+    mul1=x2*y2
+    while x1%y1 !=0:        #x,y의 최소공배수
+        temp= x1%y1
+        x1 = y1
+        y1 = temp
+    xygys = y1
+    xygbs=mul1/xygys
+    
+    mul2=xygbs*z2           #x,y의 최소공배수와 z의 최소공배수
+    while xygbs%z1 !=0:
+        temp= xygbs%z1
+        xygbs=z1
+        z1=temp
+    midgys2=z1
+    xyzlcm=mul2/midgys2
+    
+    return xyzlcm
+    
+print("%d %d" %(gcd(a,b,c) ,lcm(a,b,c)))
 
-mul2=midgbs1*c                #a와b의 최대공약수와 c의 최대공약수 구하는 알고리즘
-while midgys1 % c!=0:
-	temp = midgys1 % c
-	midgys1 = c
-	c=temp                     
-gys=c
-while midgbs1 %c2 !=0:		#a와b의 최소공배수와 c의 최소공배수 구하는 알고리즘(위의 c값이 바꼇으므로 다른 c2로 대체)
-	temp = midgbs1 % c2
-	midgbs1=c2
-	c2=temp
-midgys2=c2
-gbs=mul2/midgys2
-
-print("%d %d" %(gys,gbs)) 
-
+    
